c75179ec1aa9a5a715c704c8b5d2dfd9a74d0bb8
Parth Patel <parthpatel.org@gmail.com>
2013-03-05 22:49:39 +0530

    Signup validation completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,18 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :username
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  				uniqueness: { case_sensitive: false }
+
+
+  VALID_USERNAME_REGEX = /\A\w+\z/i  #Only letters, numbers & underscore
+  validates :username, presence: true, format: { with: VALID_USERNAME_REGEX },
+  				uniqueness: { case_sensitive: false }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,125 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:username) }
+
+  it { should be_valid }
+
+#..................................Presence validation........................  
+  	describe "when name is not present" do
+  		before { @user.name = " " }
+
+  		it { should_not be_valid }
+  	end
+
+  	describe "when email is not present" do
+  		before { @user.email = " " }
+
+  		it { should_not be_valid }
+  	end
+
+  	describe "when username is not present" do
+  		before { @user.username = " " }
+
+  		it { should_not be_valid }
+  	end
+
+#.......................Length Validation........................
+
+  	describe "when name is too long" do
+  		before { @user.name = "a" * 51 }
+
+  		it { should_not be_valid }
+  	end
+
+#.......................Email Format validation...............
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
+
+#........................Username Format Validation..........................
+
+  describe "when username format is invalid" do
+    it "should be invalid" do
+      uname = %w[.123 LK/p\_123 @klf* l-p]
+      uname.each do |invalid_uname|
+        @user.username = invalid_uname
+        @user.should_not be_valid
+      end      
+    end
+  end
+
+  describe "when username format is valid" do
+    it "should be valid" do
+      uname = %w[123 _tfg Tgr_]
+      uname.each do |valid_uname|
+        @user.username = valid_uname
+        @user.should be_valid
+      end      
+    end   
+  end
+
+#.........................Email Uniqueness Validation...................
+
+describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+  
+#......................Username uniqueness Validaton.........................
+
+describe "when username is already taken" do
+    before do
+      user_with_same_username = @user.dup
+      user_with_same_username.email = "example@user.com"
+      user_with_same_username.save
+    end
+
+    it { should_not be_valid }
+  end
+
+describe "when username is already taken" do
+    before do
+      user_with_same_username = @user.dup
+      user_with_same_username.username = @user.username.upcase
+      user_with_same_username.email = "example@user.com"
+      user_with_same_username.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  
 end
+
