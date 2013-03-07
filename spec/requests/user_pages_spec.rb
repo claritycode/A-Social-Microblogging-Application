79cb018d109e5217791f9a1cfe99aad00808617e
Parth Patel <parthpatel.org@gmail.com>
2013-03-07 11:04:51 +0530

    SignUp page error messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,43 @@ require 'spec_helper'
 describe "User Pages" do
   subject { page }
 
-	describe "signup page" do
-	    before { visit signup_path }
+  	describe "signup page" do
+  		before { visit signup_path }
 
-	    it { should have_selector('h1',    text: 'Sign up') }
+  		it { should have_selector('h1',    text: 'Sign up') }
 	    it { should have_selector('title', text: full_title('Sign up')) }
-   	end
+  	end
+
+	describe "signup" do
+	    before { visit signup_path }
+	    let(:submit) { "Create my account" }  
+
+	    describe "with invalid information" do
+	    	it "should not create valid user" do
+	    		expect { click_button submit }.not_to change(User, :count) #Capybara Syntax
+	    	end
+	     end
+
+	    describe "with valid information" do
+	    	before do 
+	    		fill_in "Name", with: "Example User"
+	    		fill_in "Email", with: "user@example.com"
+	    		fill_in "Username", with: "uexample"
+	    		fill_in "Password", with: "project8"
+	    		fill_in "Confirmation", with: "project8"
+	    	end
+	    	it "should create valid user" do
+	    		expect { click_button submit }.to change(User, :count).by(1) #Capybara Syntax
+	    	end
+	    end
+
+	    describe "after submission" do
+	        before { click_button submit }
+
+	        it { should have_selector('title', text: 'Sign up') }
+	        it { should have_content('error') }
+      end
+	end
 
 	describe "profile page" do 
 		let(:user) { FactoryGirl.create(:user) }
