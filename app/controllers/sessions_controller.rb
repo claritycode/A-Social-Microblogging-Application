f2127ff4333cbccd5e617ec7bbcd4274a9a0d0f9
Parth Patel <parthpatel.org@gmail.com>
2013-03-08 11:32:56 +0530

    Completed with failed sign in scenario

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+
+		else
+			flash.now[:error] = "Invalid Email/password Combination"
+		 	render 'new'
+		 end
 	end
 
 	def destroy
