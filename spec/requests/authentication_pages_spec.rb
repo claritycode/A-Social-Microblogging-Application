904b7e5c90c905de15b97a01945d01d2c4e6d342
Parth Patel <parthpatel.org@gmail.com>
2013-03-08 10:47:38 +0530

    Improvement to Sign In page and few test on it

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,4 +7,29 @@ describe "Authentication" do
 
 	it { should have_selector('h1', text: 'Sign In') }
 	it { should have_selector('title', text: full_title('Sign In')) }
+
+	describe "signin" do
+		
+#..................Check for error on invalid data.................
+		describe "with invalid information" do
+			before { click_button 'Sign In' }
+
+			it { should have_selector('title', text: full_title('Sign In')) }
+			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+		end
+
+		describe "with valid information" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				fill_in "Email", with: user.email.upcase
+				fill_in "Password", with: user.password
+				click_button "Sign In"
+			end
+
+			it { should have_selector('title', text: user.name) }
+			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Sign out', href: signout_path) }
+			it { should_not have_link('Sign In', href: signin_path) }
+		end
+	end
 end
