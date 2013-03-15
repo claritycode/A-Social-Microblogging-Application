75f0b6d0d3b97085bf22196c28fe2ba068d73493
Parth Patel <parthpatel.org@gmail.com>
2013-03-15 18:42:57 +0530

    Created view for micropost using partials

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,10 +54,22 @@ describe "User Pages" do
 
   	  describe "profile page" do 
     		let(:user) { FactoryGirl.create(:user) }
+        let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Test") }
+        let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Beta") }
     		before { visit user_path(user) }
 
     		it { should have_selector('h1', text: user.name) }
     		it { should have_selector('title', text: full_title(user.name)) }
+
+        #........Microposts..................
+
+        describe "microposts" do
+
+          it { should have_content(m1.content) }
+          it { should have_content(m2.content) }
+          it { should have_content(user.microposts.count) }
+        end
+
      	end
 
      	describe "edit" do
