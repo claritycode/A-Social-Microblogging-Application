3d3c0452f5ddcf614721bc5d4ca28c9a859b5bd4
Parth Patel <parthpatel.org@gmail.com>
2013-03-19 00:38:02 +0530

    Created Followed User association

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :username, :password, :password_confirmation
   has_many :microposts, dependent: :destroy #destroyes the dependent microposts
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  
+  has_many :followed_users, through: :relationships, source: :followed
+
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
@@ -40,6 +41,18 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,9 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) } #Micropost model
   it { should respond_to(:relationships) } #Relationship Model
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:following?) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -248,6 +251,26 @@ describe "when username is already taken" do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save!
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "unfollowing" do
+      before do
+        @user.unfollow!(other_user)
+      end
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
