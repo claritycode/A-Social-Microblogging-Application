719c58817eecaf04b023ad287890c5d0dab394d2
Parth Patel <parthpatel.org@gmail.com>
2013-04-12 19:00:21 +0530

    Completed with forgot-password, done edit and  action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,5 +9,23 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  	@user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def update
+  	@user = User.find_by_password_reset_token!(params[:id])
+  	@user.updating_password = true if @user
+    if @user.password_reset_sent_at < 2.hours.ago
+      flash[:notice] = 'Password Reset Request has expired. Please try again'
+      redirect_to new_password_reset_path
+  	elsif @user.update_attributes(params[:user])
+      @user.password_reset_sent_at = 3.hours.ago
+      @user.save
+      flash[:success] = 'Your Password Reset was successful. 
+      Please Sign In to ontinue enjoying the service'
+  		redirect_to signin_url
+  	else
+  		render 'edit'
+  	end
   end
 end
--- spec/requests/password_reset_pages_spec.rb
+++ spec/requests/password_reset_pages_spec.rb
@@ -22,4 +22,70 @@ describe "Password Reset Pages" do
 		end
 	end
 
+	describe "Edit" do
+		let(:user) { FactoryGirl.create(:user) }
+
+		before do
+			user.send_password_reset_email
+			visit edit_password_reset_path(user.password_reset_token)	
+		end
+
+		describe "Page" do
+			
+			it { should have_selector('title', text: 'Reset Password') }
+			it { should have_selector('h1', text: 'Reset Password') }
+		end
+
+		describe "with invalid information" do
+			before { click_button 'Reset Password' }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			# let!(:old_password) { "charusat" }
+			before do
+				fill_in "New Password", with: "8project"
+				fill_in "Confirm New Password", with: "8project"
+				click_button 'Reset Password'
+			end
+
+			it { should have_selector('title', text: 'Sign In') }
+			it { should have_selector('div.alert.alert-success', 
+				text: 'Your Password Reset was successful') }
+			
+			describe "should change the password" do
+				before do
+					sign_in user
+				end
+
+				it { should have_selector('title', text:'Sign In') }
+				it { should have_selector('div.alert.alert-error', 
+					text: 'Invalid') }
+			end
+
+			describe "trying to use same url again" do
+				before do
+					visit edit_password_reset_path(user.password_reset_token)
+					click_button 'Reset Password'
+				end
+
+				it { should have_selector('title', text: 'Password Reset') }
+				it { should have_selector('h1', text: 'Password Reset') }
+				it { should have_selector('div.alert.alert-notice', text: 'Password Reset Request has expired. Please try again') }				
+			end
+		end
+
+		describe "when Password reset url had already expired" do
+				before do
+					user.password_reset_sent_at = 5.hours.ago
+					user.save
+					click_button 'Reset Password'
+				end
+
+				it { should have_selector('title', text: 'Password Reset') }
+				it { should have_selector('h1', text: 'Password Reset') }
+				it { should have_selector('div.alert.alert-notice', text: 'Password Reset Request has expired. Please try again') }
+			end
+	end
 end
